f5b015ba9731e3fc0793e6e30f718f47f2631d6f
kristenbetelgeusecom <kristenbetelgereal@gmail.com>
2023-04-17 19:47:44 +0300

    init: added project

---
+++ src/queue.js
@@ -0,0 +1,44 @@
+const { NotImplementedError } = require('../extensions/index.js');
+
+const { ListNode } = require('../extensions/list-node.js');
+
+/**
+ * Implement the Queue with a given interface via linked list (use ListNode extension above).
+ *
+ * @example
+ * const queue = new Queue();
+ *
+ * queue.enqueue(1); // adds the element to the queue
+ * queue.enqueue(3); // adds the element to the queue
+ * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+ * queue.getUnderlyingList() // returns { value: 3, next: null }
+ */
+class Queue {
+  constructor() {
+    this.elements = {};
+    this.head = 0;
+    this.tail = 0;
+    this.length = 0;
+  }
+
+  getUnderlyingList() {
+    throw new NotImplementedError('Not implemented');
+    //return this.elements
+  }
+
+  enqueue(element) {
+    this.elements[this.tail] = element;
+    this.tail++;
+  }
+
+  dequeue() {
+    const item = this.elements[this.head];
+    delete this.elements[this.head];
+    this.head++;
+    return item;
+  }
+}
+
+module.exports = {
+  Queue
+};
